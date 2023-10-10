7bf89564b2285c0a8de18724fd891c5f0227d624
Hamid <abdulbarinijrabi@ABDULs-MBP.hsd1.va.comcast.net>
2023-10-10 13:01:34 -0400

    solve challenge 4 and 5

--- src/main/java/Solutions.java
+++ src/main/java/Solutions.java
@@ -20,6 +20,24 @@ public class Solutions {
         //Person{id=4, name='Chris', gender=MALE, dob=1993-12-16, income=1800.0}
         //Person{id=5, name='Laynie', gender=FEMALE, dob=2012-12-13, income=0.0}
         //-> here we sorted based on ncome and by desinding (big to small).
+
+        //4. **Distinct (Intermediate Operation):**
+        //    - Find the distinct genders in the list of persons.
+        System.out.println("Distinct genders are: " +distinctGendrs());
+        //output:
+        //Distinct genders are: [MALE, FEMALE]
+
+
+
+        //5. **Limit (Intermediate Operation):**
+        //    - Limit the list of persons to the first 3.
+        System.out.println(" First three people on the top of the list are: ");
+        firstThreePeople().forEach(System.out::println);
+        //output:
+        //First three people on the top of the list are:
+        //Person{id=1, name='Ken', gender=MALE, dob=1970-05-04, income=6000.0}
+        //Person{id=2, name='Jeff', gender=MALE, dob=1970-07-15, income=7100.0}
+        //Person{id=3, name='Donna', gender=FEMALE, dob=1962-07-29, income=8700.0}
     }
     // create method for chanllege 1
 static List<String> malesOnly(List<Person> people){
@@ -33,8 +51,27 @@ static List<String> malesOnly(List<Person> people){
     static List<Person> sortedByIncomeDesc(){
         List<Person> sortedList = Person.persons()
                 .stream()
-                .sorted(Comparator.comparing(Person::getIncome).reversed())
+                .sorted(Comparator.comparing(Person::getIncome).reversed())//here
+                //we use comparator with sort cuz it is custom object of Person.
                 .toList();
         return sortedList;
+    }
+    // #4 challenge method
+    static List<Person.Gender> distinctGendrs(){
+        List<Person.Gender> genders = Person.persons()
+                .stream()
+                .map(Person::getGender)
+                .distinct()// use this method cuz we dont want list of all males and females
+                // we only want to know we have how many gender (male and female)
+                .toList();
+        return genders;
+    }
+    // method for challenge 5
+    static  List<Person> firstThreePeople(){
+        List<Person> top3 = Person.persons()
+                .stream()
+                .limit(3)// cuz we only want to see top 3 peopke
+                .toList();
+        return top3;
     }
 }
